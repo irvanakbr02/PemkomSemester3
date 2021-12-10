edb0d5b675ca35949a868c23bfc75cc3cc64327f
irvanakbr02 <irvanfebriansyah21@gmail.com>
2021-12-10 21:11:06 +0700

    pemkom

---
+++ 19090099_Irvan_Akbar_Febriansyah/UTS-Irvan-3A/src/UTS/Smartphone.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UTS;
+
+/**
+ *
+ * @author Pengguna
+ */
+public class Smartphone {
+    private int id ;
+    private String brand;
+    private String model;
+    private int ram;
+    private int rom;
+    private String chipset;
+    private int battery;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getRam() {
+        return ram;
+    }
+
+    public void setRam(int ram) {
+        this.ram = ram;
+    }
+
+    public int getRom() {
+        return rom;
+    }
+
+    public void setRom(int rom) {
+        this.rom = rom;
+    }
+
+    public String getChipset() {
+        return chipset;
+    }
+
+    public void setChipset(String chipset) {
+        this.chipset = chipset;
+    }
+
+    public int getBattery() {
+        return battery;
+    }
+
+    public void setBattery(int battery) {
+        this.battery = battery;
+    }
+    
+    
+}
